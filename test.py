e333e9762f99970df96e71291fa736ee16fc0584
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-09-09 15:06:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+#--------------------------------------------#
+#   该部分代码只用于看网络结构，并非测试代码
+#   map测试请看get_dr_txt.py、get_gt_txt.py
+#   和get_map.py
+#--------------------------------------------#
 from nets.yolo4 import yolo_body
 from tensorflow.keras.layers import Input
+
 inputs = Input([416,416,3])
 model = yolo_body(inputs,3,80)
 model.summary()
