e28956726935fb58edb4ce126a074bc6df09709c
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-05-21 13:12:07 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-import os
+
+from functools import partial
 
 import numpy as np
 import tensorflow as tf
@@ -7,11 +8,13 @@ from tensorflow.keras.callbacks import (EarlyStopping, ReduceLROnPlateau,
 from tensorflow.keras.layers import Input, Lambda
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
+from tqdm import tqdm
 
 from nets.loss import yolo_loss
 from nets.yolo4 import yolo_body
-from utils.utils import (ModelCheckpoint, WarmUpCosineDecayScheduler, LossHistory, 
-                         get_random_data, get_random_data_with_Mosaic)
+from utils.utils import (LossHistory, ModelCheckpoint,
+                         WarmUpCosineDecayScheduler, get_random_data,
+                         get_random_data_with_Mosaic)
 
 
 #---------------------------------------------------#
@@ -34,7 +37,7 @@ def get_anchors(anchors_path):
 #---------------------------------------------------#
 #   训练数据生成器
 #---------------------------------------------------#
-def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes, mosaic=False, random=True):
+def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes, mosaic=False, random=True, eager=True):
     n = len(annotation_lines)
     i = 0
     flag = True
@@ -60,8 +63,10 @@ def data_generator(annotation_lines, batch_size, input_shape, anchors, num_class
         image_data = np.array(image_data)
         box_data = np.array(box_data)
         y_true = preprocess_true_boxes(box_data, input_shape, anchors, num_classes)
-        yield [image_data, *y_true], np.zeros(batch_size)
-
+        if eager:
+            yield image_data, y_true[0], y_true[1], y_true[2]
+        else:
+            yield [image_data, *y_true], np.zeros(batch_size)
 
 #---------------------------------------------------#
 #   读入xml文件，并输出y_true
@@ -177,6 +182,71 @@ def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes):
 
     return y_true
 
+# 防止bug
+def get_train_step_fn():
+    @tf.function
+    def train_step(imgs, yolo_loss, targets, net, optimizer, regularization, normalize):
+        with tf.GradientTape() as tape:
+            # 计算loss
+            P5_output, P4_output, P3_output = net(imgs, training=True)
+            args = [P5_output, P4_output, P3_output] + targets
+            loss_value = yolo_loss(args,anchors,num_classes,label_smoothing=label_smoothing,normalize=normalize)
+            if regularization:
+                # 加入正则化损失
+                loss_value = tf.reduce_sum(net.losses) + loss_value
+        grads = tape.gradient(loss_value, net.trainable_variables)
+        optimizer.apply_gradients(zip(grads, net.trainable_variables))
+        return loss_value
+    return train_step
+
+def fit_one_epoch(net, yolo_loss, optimizer, epoch, epoch_size, epoch_size_val, gen, genval, Epoch, anchors, 
+                        num_classes, label_smoothing, regularization=False, train_step=None):
+    loss = 0
+    val_loss = 0
+    print('Start Train')
+    with tqdm(total=epoch_size,desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
+        for iteration, batch in enumerate(gen):
+            if iteration>=epoch_size:
+                break
+            images, target0, target1, target2 = batch[0], batch[1], batch[2], batch[3]
+            targets = [target0, target1, target2]
+            targets = [tf.convert_to_tensor(target) for target in targets]
+            loss_value = train_step(images, yolo_loss, targets, net, optimizer, regularization, normalize)
+            loss = loss + loss_value
+
+            pbar.set_postfix(**{'total_loss': float(loss) / (iteration + 1), 
+                                'lr'        : optimizer._decayed_lr(tf.float32).numpy()})
+            pbar.update(1)
+            
+    print('Start Validation')
+    with tqdm(total=epoch_size_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
+        for iteration, batch in enumerate(genval):
+            if iteration>=epoch_size_val:
+                break
+            # 计算验证集loss
+            images, target0, target1, target2 = batch[0], batch[1], batch[2], batch[3]
+            targets = [target0, target1, target2]
+            targets = [tf.convert_to_tensor(target) for target in targets]
+
+            P5_output, P4_output, P3_output = net(images)
+            args = [P5_output, P4_output, P3_output] + targets
+            loss_value = yolo_loss(args,anchors,num_classes,label_smoothing=label_smoothing, normalize=normalize)
+            if regularization:
+                # 加入正则化损失
+                loss_value = tf.reduce_sum(net.losses) + loss_value
+            # 更新验证集loss
+            val_loss = val_loss + loss_value
+
+            pbar.set_postfix(**{'total_loss': float(val_loss)/ (iteration + 1)})
+            pbar.update(1)
+
+    logs = {'loss': loss.numpy()/(epoch_size+1), 'val_loss': val_loss.numpy()/(epoch_size_val+1)}
+    loss_history.on_epoch_end([], logs)
+    print('Finish Validation')
+    print('Epoch:'+ str(epoch+1) + '/' + str(Epoch))
+    print('Total Loss: %.4f || Val Loss: %.4f ' % (loss/(epoch_size+1),val_loss/(epoch_size_val+1)))
+    net.save_weights('logs/Epoch%d-Total_Loss%.4f-Val_Loss%.4f.h5'%((epoch+1),loss/(epoch_size+1),val_loss/(epoch_size_val+1)))
+
 gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
@@ -186,6 +256,10 @@ for gpu in gpus:
 #   https://www.bilibili.com/video/BV1zE411u7Vw
 #----------------------------------------------------#
 if __name__ == "__main__":
+    #----------------------------------------------------#
+    #   是否使用eager模式训练
+    #----------------------------------------------------#
+    eager = False
     #----------------------------------------------------#
     #   获得图片路径和标签
     #----------------------------------------------------#
@@ -216,17 +290,6 @@ if __name__ == "__main__":
     #   用于决定计算最终loss是除上batch_size还是除上正样本数量
     #------------------------------------------------------#
     normalize = False
-
-    #----------------------------------------------------#
-    #   获取classes和anchor
-    #----------------------------------------------------#
-    class_names = get_classes(classes_path)
-    anchors = get_anchors(anchors_path)
-    #------------------------------------------------------#
-    #   一共有多少类和多少先验框
-    #------------------------------------------------------#
-    num_classes = len(class_names)
-    num_anchors = len(anchors)
     #------------------------------------------------------#
     #   Yolov4的tricks应用
     #   mosaic 马赛克数据增强 True or False 
@@ -238,6 +301,20 @@ if __name__ == "__main__":
     Cosine_scheduler = False
     label_smoothing = 0
 
+    #------------------------------------------------------#
+    #   在eager模式下是否进行正则化
+    #------------------------------------------------------#
+    regularization = True
+    #----------------------------------------------------#
+    #   获取classes和anchor
+    #----------------------------------------------------#
+    class_names = get_classes(classes_path)
+    anchors = get_anchors(anchors_path)
+    #------------------------------------------------------#
+    #   一共有多少类和多少先验框
+    #------------------------------------------------------#
+    num_classes = len(class_names)
+    num_anchors = len(anchors)
     #------------------------------------------------------#
     #   创建yolo模型
     #------------------------------------------------------#
@@ -302,90 +379,133 @@ if __name__ == "__main__":
     for i in range(freeze_layers): model_body.layers[i].trainable = False
     print('Freeze the first {} layers of total {} layers.'.format(freeze_layers, len(model_body.layers)))
 
-    # 调整非主干模型first
+    #------------------------------------------------------#
+    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
+    #   也可以在训练初期防止权值被破坏。
+    #   Init_Epoch为起始世代
+    #   Freeze_Epoch为冻结训练的世代
+    #   Epoch总训练世代
+    #   提示OOM或者显存不足请调小Batch_size
+    #------------------------------------------------------#
     if True:
-        Init_epoch = 0
-        Freeze_epoch = 50
-        batch_size = 2
-        learning_rate_base = 1e-3
-
-        if Cosine_scheduler:
-            # 预热期
-            warmup_epoch = int((Freeze_epoch-Init_epoch)*0.2)
-            # 总共的步长
-            total_steps = int((Freeze_epoch-Init_epoch) * num_train / batch_size)
-            # 预热步长
-            warmup_steps = int(warmup_epoch * num_train / batch_size)
-            # 学习率
-            reduce_lr = WarmUpCosineDecayScheduler(learning_rate_base=learning_rate_base,
-                                                        total_steps=total_steps,
-                                                        warmup_learning_rate=1e-4,
-                                                        warmup_steps=warmup_steps,
-                                                        hold_base_rate_steps=num_train,
-                                                        min_learn_rate=1e-6
-                                                        )
-            model.compile(optimizer=Adam(), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
-        else:
-            reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1)
-            model.compile(optimizer=Adam(learning_rate_base), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
-
-        epoch_size = num_train // batch_size
-        epoch_size_val = num_val // batch_size
+        Init_epoch          = 0
+        Freeze_epoch        = 50
+        batch_size          = 2
+        learning_rate_base  = 1e-3
+        
+        epoch_size      = num_train // batch_size
+        epoch_size_val  = num_val // batch_size
 
         if epoch_size == 0 or epoch_size_val == 0:
             raise ValueError("数据集过小，无法进行训练，请扩充数据集。")
 
+        if eager:
+            gen     = tf.data.Dataset.from_generator(partial(data_generator, annotation_lines = lines[:num_train], batch_size = batch_size,
+                input_shape = input_shape, anchors = anchors, num_classes = num_classes, mosaic=mosaic, random=True), (tf.float32, tf.float32, tf.float32, tf.float32))
+            gen_val = tf.data.Dataset.from_generator(partial(data_generator, annotation_lines = lines[num_train:], batch_size = batch_size, 
+                input_shape = input_shape, anchors = anchors, num_classes = num_classes, mosaic=False, random=False), (tf.float32, tf.float32, tf.float32, tf.float32))
+
+            gen     = gen.shuffle(buffer_size=batch_size).prefetch(buffer_size=batch_size)
+            gen_val = gen_val.shuffle(buffer_size=batch_size).prefetch(buffer_size=batch_size)
+
+            if Cosine_scheduler:
+                lr_schedule = tf.keras.experimental.CosineDecayRestarts(
+                    initial_learning_rate = learning_rate_base, first_decay_steps = 5 * epoch_size, t_mul = 1.0, alpha = 1e-2)
+            else:
+                lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+                    initial_learning_rate=learning_rate_base, decay_steps=epoch_size, decay_rate=0.92, staircase=True)
+            
+            optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+        else:
+            if Cosine_scheduler:
+                # 预热期
+                warmup_epoch    = int((Freeze_epoch-Init_epoch)*0.2)
+                # 总共的步长
+                total_steps     = int((Freeze_epoch-Init_epoch) * num_train / batch_size)
+                # 预热步长
+                warmup_steps    = int(warmup_epoch * num_train / batch_size)
+                # 学习率
+                reduce_lr       = WarmUpCosineDecayScheduler(learning_rate_base=learning_rate_base, total_steps=total_steps,
+                                                            warmup_learning_rate=1e-4, warmup_steps=warmup_steps,
+                                                            hold_base_rate_steps=num_train, min_learn_rate=1e-6)
+                model.compile(optimizer=Adam(), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
+            else:
+                reduce_lr       = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1)
+                model.compile(optimizer=Adam(learning_rate_base), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
+
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit(data_generator(lines[:num_train], batch_size, input_shape, anchors, num_classes, mosaic=mosaic, random=True),
-                steps_per_epoch=epoch_size,
-                validation_data=data_generator(lines[num_train:], batch_size, input_shape, anchors, num_classes, mosaic=False, random=False),
-                validation_steps=epoch_size_val,
-                epochs=Freeze_epoch,
-                initial_epoch=Init_epoch,
-                callbacks=[logging, checkpoint, reduce_lr, early_stopping, loss_history])
-        model.save_weights(log_dir + 'trained_weights_stage_1.h5')
+        if eager:
+            for epoch in range(Init_epoch,Freeze_epoch):
+                fit_one_epoch(model_body, yolo_loss, optimizer, epoch, epoch_size, epoch_size_val,gen, gen_val, 
+                            Freeze_epoch, anchors, num_classes, label_smoothing, regularization, get_train_step_fn())
+        else:
+            model.fit(data_generator(lines[:num_train], batch_size, input_shape, anchors, num_classes, mosaic=mosaic, random=True, eager=False),
+                    steps_per_epoch=epoch_size,
+                    validation_data=data_generator(lines[num_train:], batch_size, input_shape, anchors, num_classes, mosaic=False, random=False, eager=False),
+                    validation_steps=epoch_size_val,
+                    epochs=Freeze_epoch,
+                    initial_epoch=Init_epoch,
+                    callbacks=[logging, checkpoint, reduce_lr, early_stopping, loss_history])
 
     for i in range(freeze_layers): model_body.layers[i].trainable = True
 
     # 解冻后训练
     if True:
-        Freeze_epoch = 50
-        Epoch = 100
-        batch_size = 2
-        learning_rate_base = 1e-4
+        Freeze_epoch        = 50
+        Epoch               = 100
+        batch_size          = 2
+        learning_rate_base  = 1e-4
 
-        if Cosine_scheduler:
-            # 预热期
-            warmup_epoch = int((Epoch-Freeze_epoch)*0.2)
-            # 总共的步长
-            total_steps = int((Epoch-Freeze_epoch) * num_train / batch_size)
-            # 预热步长
-            warmup_steps = int(warmup_epoch * num_train / batch_size)
-            # 学习率
-            reduce_lr = WarmUpCosineDecayScheduler(learning_rate_base=learning_rate_base,
-                                                        total_steps=total_steps,
-                                                        warmup_learning_rate=1e-5,
-                                                        warmup_steps=warmup_steps,
-                                                        hold_base_rate_steps=num_train//2,
-                                                        min_learn_rate=1e-6
-                                                        )
-            model.compile(optimizer=Adam(), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
+        if eager:
+            gen     = tf.data.Dataset.from_generator(partial(data_generator, annotation_lines = lines[:num_train], batch_size = batch_size,
+                input_shape = input_shape, anchors = anchors, num_classes = num_classes, mosaic=mosaic, random=True), (tf.float32, tf.float32, tf.float32, tf.float32))
+            gen_val = tf.data.Dataset.from_generator(partial(data_generator, annotation_lines = lines[num_train:], batch_size = batch_size, 
+                input_shape = input_shape, anchors = anchors, num_classes = num_classes, mosaic=False, random=False), (tf.float32, tf.float32, tf.float32, tf.float32))
+
+            gen     = gen.shuffle(buffer_size=batch_size).prefetch(buffer_size=batch_size)
+            gen_val = gen_val.shuffle(buffer_size=batch_size).prefetch(buffer_size=batch_size)
+
+            if Cosine_scheduler:
+                lr_schedule = tf.keras.experimental.CosineDecayRestarts(
+                    initial_learning_rate = learning_rate_base, first_decay_steps = 5 * epoch_size, t_mul = 1.0, alpha = 1e-2)
+            else:
+                lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+                    initial_learning_rate=learning_rate_base, decay_steps=epoch_size, decay_rate=0.92, staircase=True)
+            
+            optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
         else:
-            reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1)
-            model.compile(optimizer=Adam(learning_rate_base), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
+            if Cosine_scheduler:
+                # 预热期
+                warmup_epoch    = int((Epoch-Freeze_epoch)*0.2)
+                # 总共的步长
+                total_steps     = int((Epoch-Freeze_epoch) * num_train / batch_size)
+                # 预热步长
+                warmup_steps    = int(warmup_epoch * num_train / batch_size)
+                # 学习率
+                reduce_lr       = WarmUpCosineDecayScheduler(learning_rate_base=learning_rate_base, total_steps=total_steps,
+                                                            warmup_learning_rate=1e-4, warmup_steps=warmup_steps,
+                                                            hold_base_rate_steps=num_train, min_learn_rate=1e-6)
+                model.compile(optimizer=Adam(), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
+            else:
+                reduce_lr       = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1)
+                model.compile(optimizer=Adam(learning_rate_base), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
 
-        epoch_size = num_train // batch_size
-        epoch_size_val = num_val // batch_size
+        epoch_size      = num_train // batch_size
+        epoch_size_val  = num_val // batch_size
 
         if epoch_size == 0 or epoch_size_val == 0:
             raise ValueError("数据集过小，无法进行训练，请扩充数据集。")
 
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit(data_generator(lines[:num_train], batch_size, input_shape, anchors, num_classes, mosaic=mosaic, random=True),
-                steps_per_epoch=epoch_size,
-                validation_data=data_generator(lines[num_train:], batch_size, input_shape, anchors, num_classes, mosaic=False, random=False),
-                validation_steps=epoch_size_val,
-                epochs=Epoch,
-                initial_epoch=Freeze_epoch,
-                callbacks=[logging, checkpoint, reduce_lr, early_stopping, loss_history])
-        model.save_weights(log_dir + 'last1.h5')
+        if eager:
+            for epoch in range(Init_epoch,Freeze_epoch):
+                fit_one_epoch(model_body, yolo_loss, optimizer, epoch, epoch_size, epoch_size_val,gen, gen_val, 
+                            Epoch, anchors, num_classes, label_smoothing, regularization, get_train_step_fn())
+        else:
+            model.fit(data_generator(lines[:num_train], batch_size, input_shape, anchors, num_classes, mosaic=mosaic, random=True, eager=False),
+                    steps_per_epoch=epoch_size,
+                    validation_data=data_generator(lines[num_train:], batch_size, input_shape, anchors, num_classes, mosaic=False, random=False, eager=False),
+                    validation_steps=epoch_size_val,
+                    epochs=Epoch,
+                    initial_epoch=Freeze_epoch,
+                    callbacks=[logging, checkpoint, reduce_lr, early_stopping, loss_history])
--- yolo.py
+++ yolo.py
@@ -1,14 +1,12 @@
 import colorsys
-import copy
 import os
-from timeit import default_timer as timer
+import time
 
 import numpy as np
 import tensorflow as tf
 from PIL import Image, ImageDraw, ImageFont
-from tensorflow.compat.v1.keras import backend as K
 from tensorflow.keras.layers import Input, Lambda
-from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import Model
 
 from nets.yolo4 import yolo_body, yolo_eval
 from utils.utils import letterbox_image
@@ -27,10 +25,11 @@ class YOLO(object):
         "classes_path"      : 'model_data/coco_classes.txt',
         "score"             : 0.5,
         "iou"               : 0.3,
-        "eager"             : True,
         "max_boxes"         : 100,
-        # 显存比较小可以使用416x416
-        # 显存比较大可以使用608x608
+        #-------------------------------#
+        #   显存比较小可以使用416x416
+        #   显存比较大可以使用608x608
+        #-------------------------------#
         "model_image_size"  : (416, 416),
         #---------------------------------------------------------------------#
         #   该变量用于控制是否使用letterbox_image对输入图像进行不失真的resize，
@@ -51,9 +50,6 @@ class YOLO(object):
     #---------------------------------------------------#
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
-        if not self.eager:
-            tf.compat.v1.disable_eager_execution()
-            self.sess = K.get_session()
         self.class_names = self._get_class()
         self.anchors = self._get_anchors()
         self.generate()
@@ -116,19 +112,12 @@ class YOLO(object):
         #   在yolo_eval函数中，我们会对预测结果进行后处理
         #   后处理的内容包括，解码、非极大抑制、门限筛选等
         #---------------------------------------------------------#
-        if self.eager:
-            self.input_image_shape = Input([2,],batch_size=1)
-            inputs = [*self.yolo_model.output, self.input_image_shape]
-            outputs = Lambda(yolo_eval, output_shape=(1,), name='yolo_eval',
-                arguments={'anchors': self.anchors, 'num_classes': len(self.class_names), 'image_shape': self.model_image_size, 
-                'score_threshold': self.score, 'eager': True, 'max_boxes': self.max_boxes, 'letterbox_image': self.letterbox_image})(inputs)
-            self.yolo_model = Model([self.yolo_model.input, self.input_image_shape], outputs)
-        else:
-            self.input_image_shape = K.placeholder(shape=(2, ))
-            
-            self.boxes, self.scores, self.classes = yolo_eval(self.yolo_model.output, self.anchors,
-                    num_classes, self.input_image_shape, max_boxes=self.max_boxes,
-                    score_threshold=self.score, iou_threshold=self.iou, letterbox_image=self.letterbox_image)
+        self.input_image_shape = Input([2,],batch_size=1)
+        inputs = [*self.yolo_model.output, self.input_image_shape]
+        outputs = Lambda(yolo_eval, output_shape=(1,), name='yolo_eval',
+            arguments={'anchors': self.anchors, 'num_classes': len(self.class_names), 'image_shape': self.model_image_size, 
+            'score_threshold': self.score, 'eager': True, 'max_boxes': self.max_boxes, 'letterbox_image': self.letterbox_image})(inputs)
+        self.yolo_model = Model([self.yolo_model.input, self.input_image_shape], outputs)
  
     @tf.function
     def get_pred(self, image_data, input_image_shape):
@@ -159,31 +148,17 @@ class YOLO(object):
         #   添加上batch_size维度
         #---------------------------------------------------------#
         image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
-
         #---------------------------------------------------------#
         #   将图像输入网络当中进行预测！
         #---------------------------------------------------------#
-        if self.eager:
-            # 预测结果
-            input_image_shape = np.expand_dims(np.array([image.size[1], image.size[0]], dtype='float32'), 0)
-            out_boxes, out_scores, out_classes = self.get_pred(image_data, input_image_shape) 
-        else:
-            # 预测结果
-            out_boxes, out_scores, out_classes = self.sess.run(
-                [self.boxes, self.scores, self.classes],
-                feed_dict={
-                    self.yolo_model.input: image_data,
-                    self.input_image_shape: [image.size[1], image.size[0]],
-                    K.learning_phase(): 0
-                })
+        input_image_shape = np.expand_dims(np.array([image.size[1], image.size[0]], dtype='float32'), 0)
+        out_boxes, out_scores, out_classes = self.get_pred(image_data, input_image_shape) 
         
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
-
         #---------------------------------------------------------#
         #   设置字体
         #---------------------------------------------------------#
-        font = ImageFont.truetype(font='model_data/simhei.ttf',
-                    size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+        font = ImageFont.truetype(font='model_data/simhei.ttf', size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
         thickness = max((image.size[0] + image.size[1]) // 300, 1)
         
         for i, c in list(enumerate(out_classes)):
@@ -225,5 +200,36 @@ class YOLO(object):
 
         return image
 
-    def close_session(self):
-        self.sess.close()
+    def get_FPS(self, image, test_interval):
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #   也可以直接resize进行识别
+        #---------------------------------------------------------#
+        if self.letterbox_image:
+            boxed_image = letterbox_image(image, (self.model_image_size[1],self.model_image_size[0]))
+        else:
+            boxed_image = image.convert('RGB')
+            boxed_image = boxed_image.resize((self.model_image_size[1],self.model_image_size[0]), Image.BICUBIC)
+        image_data = np.array(boxed_image, dtype='float32')
+        image_data /= 255.
+        #---------------------------------------------------------#
+        #   添加上batch_size维度
+        #---------------------------------------------------------#
+        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+
+        #---------------------------------------------------------#
+        #   将图像输入网络当中进行预测！
+        #---------------------------------------------------------#
+        input_image_shape = np.expand_dims(np.array([image.size[1], image.size[0]], dtype='float32'), 0)
+        out_boxes, out_scores, out_classes = self.get_pred(image_data, input_image_shape) 
+
+        t1 = time.time()
+        for _ in range(test_interval):
+            #---------------------------------------------------------#
+            #   将图像输入网络当中进行预测！
+            #---------------------------------------------------------#
+            input_image_shape = np.expand_dims(np.array([image.size[1], image.size[0]], dtype='float32'), 0)
+            out_boxes, out_scores, out_classes = self.get_pred(image_data, input_image_shape) 
+        t2 = time.time()
+        tact_time = (t2 - t1) / test_interval
+        return tact_time
