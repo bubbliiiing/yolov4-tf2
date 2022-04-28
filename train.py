57762a61cc8779085baded307507506f0e5e03f4
Bubbliiiing <3323290568@qq.com>
2022-04-28 11:05:10 +0800

    fix eager mode bugs

--- train.py
+++ train.py
@@ -224,6 +224,12 @@ if __name__ == "__main__":
     gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
+    
+    #------------------------------------------------------#
+    #   判断当前使用的GPU数量与机器上实际的GPU数量
+    #------------------------------------------------------#
+    if ngpus_per_node > 1 and ngpus_per_node > len(gpus):
+        raise ValueError("The number of GPUs specified for training is more than the GPUs on the machine")
         
     if ngpus_per_node > 1:
         strategy = tf.distribute.MirroredStrategy()
