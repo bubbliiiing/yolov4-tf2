17475177de454c34bd8654bc98720d2f06fc61e0
Bubbliiiing <3323290568@qq.com>
2022-05-01 14:36:07 +0800

    update flops and training steps

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -131,5 +131,11 @@ def fit_one_epoch(net, loss_history, optimizer, epoch, epoch_step, epoch_step_va
     loss_history.on_epoch_end([], logs)
     print('Epoch:'+ str(epoch+1) + '/' + str(Epoch))
     print('Total Loss: %.3f || Val Loss: %.3f ' % (loss / epoch_step, val_loss / epoch_step_val))
+    
+    #-----------------------------------------------#
+    #   保存权值
+    #-----------------------------------------------#
     if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
         net.save_weights(os.path.join(save_dir, "ep%03d-loss%.3f-val_loss%.3f.h5" % (epoch + 1, loss / epoch_step, val_loss / epoch_step_val)))
+
+    net.save_weights(os.path.join(save_dir, "last_epoch_weights.h5"))
